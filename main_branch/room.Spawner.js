d9ebc71c7d8feb2e8df621efecad04ad7aa4c0eb
mperrdev <31925457+mperrdev@users.noreply.github.com>
2017-09-13 16:08:55 -0500

    s

--- main_branch/room.Spawner.js
+++ main_branch/room.Spawner.js
@@ -4,7 +4,7 @@ require('role.upgrader');
 module.exports = {
     run: function(spawner) {
         if (spawner.energy == spawner.energyCapacity) {
-            var creepName = spawner.createCreep( [WORK,CARRY,CARRY,MOVE], undefined, {working: false, role: harvester} );
+            var creepName = spawner.createCreep( [WORK,CARRY,CARRY,MOVE], undefined, {working: false, role: 'harvester'} );
             console.log("Spawning harvester creep: " + creepName);
         }
     }

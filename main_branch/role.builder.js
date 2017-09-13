228fee7f0a484e819575d6ea62ce85cdec997e9f
mperrdev <31925457+mperrdev@users.noreply.github.com>
2017-09-13 16:29:01 -0500

    s

---
+++ main_branch/role.builder.js
@@ -0,0 +1,23 @@
+module.exports = {
+    run: function(creep) {
+        if (creep.memory.working == true && creep.carry.energy == 0) {
+            creep.memory.working = false;
+        }
+        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.working = true;
+        }
+
+        if (creep.memory.working == true) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+
+            }
+        }
+        else {
+            var source = creep.pos.findClosestByPath(Game.spawns.Spawn1)
+            if (creep.withdraw(Game.spawns.Spawn1, RESOURCE_ENERGY, (creep.carryCapacity - creep.carry.energy)) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns.Spawn1);
+            }
+        }
+    }
+}
--- main_branch/room.Spawner.js
+++ main_branch/room.Spawner.js
@@ -3,19 +3,32 @@ require('role.upgrader');
 
 module.exports = {
     run: function(spawner) {
-        spawner.memory.minHarvesterCount = 10;
+
+        //Harvesters
+        spawner.memory.minHarvesterCount = 15;
         spawner.memory.harvesterCount = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
         console.log(spawner.memory.harvesterCount);
-        console.log(Game.creeps);
 
-        if(spawner.memory.harvesterCount < spawner.memory.minHarvesterCount) {
+        //Upgraders
+        spawner.memory.minUpgraderCount = 5;
+        spawner.memory.upgraderCount = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
+        console.log(spawner.memory.upgraderCount);
+
+
+        if (spawner.memory.harvesterCount < spawner.memory.minHarvesterCount) {
             if (spawner.energy == spawner.energyCapacity) {
-                var creepName = spawner.createCreep( [WORK,CARRY,CARRY,MOVE], undefined, {working: false, role: 'harvester'} );
+                var creepName = spawner.createCreep( [WORK,WORK,CARRY,MOVE], undefined, {working: false, role: 'harvester'} );
                 console.log("Spawning harvester creep: " + creepName);
             }
         }
+        else if (spawner.memory.upgraderCount < spawner.memory.minUpgraderCount) {
+            if (spawner.energy == spawner.energyCapacity) {
+                var creepName = spawner.createCreep( [WORK,CARRY,CARRY,MOVE], undefined, {working: false, role: 'upgrader'} );
+                console.log("Spawning upgrader creep: " + creepName);
+            }
+        }
         else{
-            console.log("Met harvester count in room: " + spawner.pos.roomName);
+            console.log("Met harvester and upgrader count in room: " + spawner.roomName);
         }
     }
 };

3c80a9cfbdb113f814d81a6f01c6779312f41bd7
mperrdev <31925457+mperrdev@users.noreply.github.com>
2017-09-13 15:37:35 -0500

    s

--- main_branch/role.upgrader.js
+++ main_branch/role.upgrader.js
@@ -8,7 +8,7 @@ module.exports = {
         }
 
         if (creep.memory.working == true) {
-            if (creep.upgradeController(room.controller)) {
+            if (creep.upgradeController(room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
         }

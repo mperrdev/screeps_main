b3931df6d7ef42f2e77407275483361cae3a43aa
mperrdev <31925457+mperrdev@users.noreply.github.com>
2017-09-13 15:18:40 -0500

    s

--- main_branch/main.js
+++ main_branch/main.js
@@ -1,11 +1,23 @@
-var roleHarvester = require('role.harvester');
-
 module.exports.loop = function () {
+    var creep = Game.creeps.Caroline;
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
+    if (creep.memory.working == true && creep.carry.energy == 0) {
+        creep.memory.working = false;
+    }
+    else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+        creep.memory.working = true;
+    }
+
+    if (creep.memory.working == true) {
+        if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(Game.spawns.Spawn1);
         }
     }
+    else {
+        var source = creep.pos.findClosestByPath(FIND_SOURCES)
+        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(source);
+        }
+    }
+
 }

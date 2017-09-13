84c1cecfcffd1ed0194b256acd7e5873c69ec997
mperrdev <31925457+mperrdev@users.noreply.github.com>
2017-09-13 15:11:33 -0500

    test

--- main_branch/main.js
+++ main_branch/main.js
@@ -4,8 +4,5 @@ module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
     }
 }

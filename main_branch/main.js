2485375bcc65e352509d6be27e43ee67dc1ce65d
mperrdev <31925457+mperrdev@users.noreply.github.com>
2017-09-14 01:16:14 -0500

    s

--- main_branch/main.js
+++ main_branch/main.js
@@ -1,6 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleAttacker = require('role.attacker');
 var roomSpawner = require('room.Spawner');
 
 module.exports.loop = function () {
@@ -22,6 +23,9 @@ module.exports.loop = function () {
         }
         else if (creep.memory.role == "builder") {
             roleBuilder.run(creep);
+        }
+        else if (creep.memory.role == 'attacker') {
+            roleAttacker.run(creep);
         }
     }
 

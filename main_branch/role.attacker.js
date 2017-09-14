557366bf2e8088572ca1b47b59466eae13ab8646
mperrdev <31925457+mperrdev@users.noreply.github.com>
2017-09-14 01:16:51 -0500

    s

--- main_branch/role.attacker.js
+++ main_branch/role.attacker.js
@@ -1,15 +1,11 @@
 module.exports = {
     run: function(creep) {
-
-
+        
             var attackThis = Memory.commands.attackTarget.pos
             var enemyController = attackThis.room.controller
 
             if (creep.attackController(enemyController) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(enemyController)
-            }
-
-
         }
     }
 }
